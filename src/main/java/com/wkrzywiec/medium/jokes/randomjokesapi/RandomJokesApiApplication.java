2ba91fee967a3d3174a46dfcc0f4759391006cbb
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2019-10-18 10:52:32 +0200

    init

---
+++ src/main/java/com/wkrzywiec/medium/jokes/randomjokesapi/RandomJokesApiApplication.java
@@ -0,0 +1,13 @@
+package com.wkrzywiec.medium.jokes.randomjokesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomJokesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomJokesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wkrzywiec/medium/jokes/randomjokesapi/RandomJokesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wkrzywiec.medium.jokes.randomjokesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomJokesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
